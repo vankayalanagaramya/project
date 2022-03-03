6df5e8066779d9e232a18e4a6f6e770cd6a96ee8
vankayalanagaramya <ramyav393@gmail.com>
2022-03-03 23:19:12 +0530

    added two files condition.js and function.js

--- condition.js
+++ condition.js
@@ -16,8 +16,37 @@ switch(fruit){
     case 'Mango': console.log(`King of fruits is ${fruit}!!`);
 }
 
-//inline decision using ? :
-let firstName = {Ram : true};
+//ternary loop ? :
+let firstName = {Ram : false};
 let name = !firstName.Ram
 ? console.log('This is not the correct name')
 : console.log('This is the firstname');
+
+//for loop
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
+let pet='dog';
+console.log(pet.length);
+for(let i=1;i<=(pet.length);i++){
+    console.log(i);
+}
+
+//while
+let i=9;
+while(i>1){
+    console.log(i);
+    i--;
+}
+
+// traditional functions
+let a,b,c,d;
+function multiply(a,b){
+    return a*b;
+}
+function divide(c,d){
+    return c/d;
+}
+multiply(1,7);
+divide(4,2);
---
+++ function.js
@@ -0,0 +1,12 @@
+// traditional functions
+let a,b,c,d;
+function multiply(a,b){
+    console.log(a*b);
+}
+function divide(c,d){
+    console.log(c/d);
+}
+multiply(1,7);
+divide(4,2);
+
+//arrow functions
