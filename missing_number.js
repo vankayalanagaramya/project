143a0147b946b2153d8accdf625e1ca570dc84b0
vankayalanagaramya <ramyav393@gmail.com>
2022-03-04 23:30:52 +0530

    updated code in missing_number.js to work when the numbers are not sorted

--- missing_number.js
+++ missing_number.js
@@ -1,3 +1,5 @@
+/* This logic is used when the numbers in an array are in sequence
+
 let arr = [3,4,5,7];
 let n= arr.length;
 //console.log(`length= ${n}`);
@@ -9,6 +11,18 @@ for(let i=0;i<arr.length-1;i++){
 console.log(`The missing number is ${a+1}`);
     }
 }
- //   
+ */   
     
-
+//When numbers in an array are not sorted
+let arr = [1,2,3,4,5,6,7,8,9,10,11,15,13,18,12,16,17]
+let n = arr.length;
+//console.log(`no of elements in array is ${n}`);
+let sumtotal= ((n+2) *(n+1))/2;
+//console.log(`sum of n numbers is ${sumtotal}`);
+var s=0;
+for(var i=0;i<=arr.length-1;i++){
+    //console.log(`The elements in array are ${arr[i]}`);
+    s=s+arr[i];
+}
+//console.log(`after addition ${s}`);
+console.log(`Missing element in the array is ${Math.abs(sumtotal-s)}`);
