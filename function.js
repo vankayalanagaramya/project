c8b9dd85182fa367911d0e530e41fb32d405bc7f
vankayalanagaramya <ramyav393@gmail.com>
2022-03-04 20:22:40 +0530

    added new file function.js

--- function.js
+++ function.js
@@ -10,3 +10,10 @@ multiply(1,7);
 divide(4,2);
 
 //arrow functions
+const add = (a1,a2) =>{
+     console.log(a1+a2);
+}
+add(7,12);
+
+const subtract = (s1,s2) => (s1-s2);
+console.log(subtract(9,5));
