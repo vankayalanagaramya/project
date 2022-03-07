897a4be15f57b6cda6cd558c77e072e5fc902881
vankayalanagaramya <ramyav393@gmail.com>
2022-03-07 12:05:19 +0530

    in pair_sum_array.js added a function and return stmt

--- pair_sum_array.js
+++ pair_sum_array.js
@@ -2,23 +2,26 @@
 
 //pgm to calculate target sum from the pair of integers of an array
 
-let arr = [0,5,1,7,8,3];
-let n = arr.length;
+
 //let req_sum=8;
 function pairSum(arr, n, req_sum){
+    const result = [];
 for(let i=0;i<arr.length;i++){
     for(let j=i+1;j<arr.length;j++){
         if(arr[i]+arr[j] === req_sum){
-            console.log(`${arr[i]},${arr[j]}`);
+            result.push([arr[i],arr[j]]);
         }
     }
 }
-return false;
+console.log(result);
+return result;
+//console.log();
 }
-if (pairSum(arr, n, 8)){
-    console.log("valid pair exists");
+let arr = [0,5,1,7,3,8];
+let n = arr.length;
+if(pairSum(arr, n, 8)){
+console.log(`pair exists`);
 }
 else{
-    console.log("pair does not exists");
+    console.log("pair not exist");
 }
-
