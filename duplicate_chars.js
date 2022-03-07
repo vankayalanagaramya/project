265653cd163f98baac7f2dabcf49e79acf5a42a0
vankayalanagaramya <ramyav393@gmail.com>
2022-03-07 10:07:53 +0530

    added new file duplicate_chars.js

---
+++ duplicate_chars.js
@@ -0,0 +1,22 @@
+
+     
+    let NO_OF_CHARS = 256;
+    function duplicate(str)
+    {
+        let count = new Array(NO_OF_CHARS);
+        count.fill(0);
+          
+        for (let i = 0; i < str.length; i++)
+            count[str[i].charCodeAt()]++;
+             
+        for (let i = 0; i < NO_OF_CHARS; i++)
+        {
+            if(count[i] > 1)
+            {
+                console.log(String.fromCharCode(i));
+            }
+        }    
+    }
+     
+    let str = "ramya";
+      duplicate(str);
