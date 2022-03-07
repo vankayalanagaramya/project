fe063d7e018755dbad07eb0dc002f0c22c7834ff
vankayalanagaramya <ramyav393@gmail.com>
2022-03-07 17:56:16 +0530

    created new file subarray_sum.js to find the subarray which satisfies the target sum, added a function

--- subarray_sum.js
+++ subarray_sum.js
@@ -1,11 +1,15 @@
 let arr = [0,2,3,-5,6,-2,1];
 let n =arr.length;
+function subArray(arr,n){
 for(let i=0;i<n;i++){
     let sum = 0;
     for(j=i;j<n;j++){
         sum=sum+arr[j];
-        if(sum===5){
+        if(sum===0){
             console.log(arr.slice(i,j+1));
         }
     }
 }
+}
+
+subArray(arr,n);
