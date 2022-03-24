a2757ccd8bef80a70e02e7a46929e071d5b9f5df
vankayalanagaramya <ramyav393@gmail.com>
2022-03-24 12:23:08 +0530

    added new file queue_basics.js to understand Q basics

---
+++ queue_basics.js
@@ -0,0 +1,11 @@
+let queue=[];
+
+//push inserts new element to queue
+queue.push(8,2,5,3);
+//unshift also inserts elements to queue
+queue.unshift(1,3);
+console.log(queue);
+
+//shift deletes the first element from queue
+queue.shift();
+console.log(queue);
